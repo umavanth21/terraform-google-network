55c5e186f3d6f573192e32f1a907d74d7dd0970a
umavanth21 <56533761+umavanth21@users.noreply.github.com>
2020-06-10 12:03:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ module "subnets" {
   project_id       = var.project_id
   network_name     = module.vpc.network_name
   subnets          = var.subnets
+	 //subnets          = var.subnets
   secondary_ranges = var.secondary_ranges
 }
 
